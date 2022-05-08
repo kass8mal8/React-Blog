b3e356a0e652347a81e910510cac166ac886b688
kass8mal8 <kassimaly21@gmail.com>
2022-05-08 08:51:45 +0300

    1 file modified

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,16 @@
 import React from "react"
 import "./style"
+import {useState} from 'react'
 const Search =()=>{
+  const [placeholder, setPlaceholder]=useState("enter name of blog")
+  const handleSearch =()=>{
+    
+  }
   return(
     <div className="searchBlog">
-      <input type="text" plaeholder="search blog" />
-      <button>search</button> 
+      <input type="text" value={placeholder}
+      onChange={e=>setPlaceholder(e.target.value)} />
+      <button onClick ={handleSearch }>search</button> 
     </div>
   )
 }
