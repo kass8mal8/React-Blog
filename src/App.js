32d014f47b5bb2ed419123726026d174932eafb3
kass8mal8 <kassimaly21@gmail.com>
2022-05-08 20:27:22 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./style.css";
 import Home from "./Home"
 import Blogs from "./Blogs";
+import {BrowserRouter,Routes, Route } from "react-router-dom"
 
 export default function App() {
   return (
     <div>
-      <Blogs />
+       <Routes>
+          <Route path="/" element={ <Blogs />} />
+       </Routes>
     </div>
   );
 }
