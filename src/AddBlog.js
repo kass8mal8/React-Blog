00961fc2e5f7d54bda3ec30d794325300f4f25b6
kass8mal8 <kassimaly21@gmail.com>
2022-05-08 09:03:12 +0300

    2 files modified, 1 added

---
+++ src/AddBlog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const AddBlog=()=>{
+  return(
+    <div className="addBlog">
+        <button>add new</button>
+    </div>
+  )
+}
+export default AddBlog
--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,18 @@
 import React from "react"
 import "./style"
 import {useState} from 'react'
+import AddBlog from "./AddBlog"
 const Search =()=>{
   const [placeholder, setPlaceholder]=useState("enter name of blog")
   const handleSearch =()=>{
-    
+
   }
   return(
     <div className="searchBlog">
       <input type="text" value={placeholder}
       onChange={e=>setPlaceholder(e.target.value)} />
-      <button onClick ={handleSearch }>search</button> 
+      <button onClick ={handleSearch }>search</button>
+        <AddBlog />
     </div>
   )
 }
