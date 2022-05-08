856a2eac26034c1a8e43a584e0e7213abab688c0
kass8mal8 <kassimaly21@gmail.com>
2022-05-08 08:31:29 +0300

    1 file modified

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,19 @@
 import React from "react"
 import BlogList from "./BlogList"
 import Search from "./Search"
+import useState from  'rect-router-dom'
 
 const Home=()=>{
-  const blogs=[
+  const [blogs, setBlogs] =useState ([
     {title:"New Twitter Face",author:"Yoshi",id:1},
     {title:"Mars Journey",author:"Mario",id:2},
-    {title:"A Part Together",author:"Yoshi",id:2}
-
-  ]
+    {title:"A Part Together",author:"Yoshi",id:2} 
+  ]) 
+  
 
   const handleDelete=(id)=>{
-    blogs.filter(blog=> blog.id !== id)
+    const remBlogs=blogs.filter(blog=> blog.id !== id)
+    setBlogs(remBlogs)
   }
   return (
     <div>
