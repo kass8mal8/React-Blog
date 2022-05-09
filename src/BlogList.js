227da499198baf238268107b2a8012db48c358f7
kass8mal8 <kassimaly21@gmail.com>
2022-05-09 22:28:14 +0300

    1 file modified

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,11 +2,16 @@ import React from 'react'
 import "./style"
 const BlogList=({blogs, handleDelete} )=>{
   
+  const image="https://i.pinimg.com/originals/8c/d3/64/8cd3649cb68490847d1a036a4c7b5cb1.jpg "
+
   return(
     <div>
       {blogs.map(blog=>
       (
         <div className="blogPreview" key={blog.id}>
+            <div className="imageDiv">
+              <img src={image} alt="web" width="00"/>
+            </div>
             <h3>{blog.title}</h3>
             <p>written on: {blog.date}</p>
             <button onClick ={()=>handleDelete(blog.id)}>delete blog</button>
