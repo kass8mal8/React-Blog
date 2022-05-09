ebd803c837b3bd3d4bda603f07d2dc73f02fc501
kass8mal8 <kassimaly21@gmail.com>
2022-05-09 19:03:35 +0300

    2 files modified

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,9 @@ const Home=()=>{
        <p>
          In this project, I've decided to go ahead and blog my Journey learning React up to writing this code. Feel free to contact me for any queries. 
        </p>
+       <p>
+         I previously made some projects but I'd not coded them in React.This is the first in React, hope it's awesome. 
+       </p>
     </div>
   )
 }
