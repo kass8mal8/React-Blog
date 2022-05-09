66fc54d5780d382e4bd743d1c8cb6a9a70afc583
kass8mal8 <kassimaly21@gmail.com>
2022-05-09 18:47:53 +0300

    2 files modified

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,12 @@
 import React, {Link} from "react"
 const Home=()=>{
   return(
-    <div>
+    <div className="homeDiv">
        <p>
          Welcome to my React project.
        </p>
        <p>
-         In this project, I've decided to go ahead and blog my Journey learning React up to writing this code. Feel free to contact me for any queries. 
+         In this project, I've decided to go ahecad and blog my Journey learning React up to writing this code. Feel free to contact me for any queries. 
        </p>
     </div>
   )
