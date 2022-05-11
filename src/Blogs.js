ccfd69316c936af7d1ec69daf8dbd616b2c6e454
kass8mal8 <kassimaly21@gmail.com>
2022-05-11 13:20:50 +0300

    1 file modified

--- src/Blogs.js
+++ src/Blogs.js
@@ -4,15 +4,14 @@ import {useState}  from  'react'
 
 const Blogs=()=>{
   const [blogs, setBlogs] =useState ([
-    {title:"Day 1:Youtube Videos ",body:"Hello", date:"1/4/2022",id:1, src:" https://i.ytimg.com/vi/AVQzfUNFudU/maxresdefault.jpg"},
-    {title:"Day 2:Sololearn Tutorials ",body:"Hello", date:"4/4/2022",id:2, src:" https://i.insider.com/60f8275d0729770012b9084d?width=600&format=jpeg&auto=webp"},
-    {title:"Day 3:Pdf Tutorials",body:"Hello", date:"8/4/2022",id:3, src:" https://i.ytimg.com/vi/vrRJor7R66A/maxresdefault.jpg"},
-    {title:"Day 4:React Project Start",body:" Hello", date:"10/4/2022",id:4, src:"https://wallpaperaccess.com/full/3949102.jpg "},
-    {title:"Day 5:It's Coming Together",body:"Hello", date:"14/4/2022",id:5, src:"https://www.whatlaptops.com/wp-content/uploads/2020/09/How-Many-Watts-Does-a-Laptop-Use-When-Charging-1-780x470.jpg "}
-
+    {title:"Day 1:Youtube Videos ",body:"Hello", date:"1/5/2022",id:1, src:" https://i.ytimg.com/vi/AVQzfUNFudU/maxresdefault.jpg"},
+    {title:"Day 2:Sololearn Tutorials ",body:"Hello", date:"4/5/2022",id:2, src:" https://i.insider.com/60f8275d0729770012b9084d?width=600&format=jpeg&auto=webp"},
+    {title:"Day 3:Pdf Tutorials",body:"Hello", date:"8/5/2022",id:3, src:" https://i.ytimg.com/vi/vrRJor7R66A/maxresdefault.jpg"},
+    {title:"Day 4:React Project Start",body:" Hello", date:"10/5/2022",id:4, src:"https://wallpaperaccess.com/full/3949102.jpg "},
+    {title:"Day 5:It's Coming Together",body:"Hello", date:"14/4/2022",id:5, src:"https://www.whatlaptops.com/wp-content/uploads/2020/09/How-Many-Watts-Does-a-Laptop-Use-When-Charging-1-780x470.jpg "},
+    {title:"Day 6",body:"Hello",date:"20/5/2022",id:6,src:"https://creativetimblog.com/blog/wp-content/uploads/2020/07/react-projects-730x410.jpg"} 
   ]) 
   
-
   const handleDelete=(id)=>{
     const remBlogs=blogs.filter(blog=> blog.id !== id)
     setBlogs(remBlogs)
