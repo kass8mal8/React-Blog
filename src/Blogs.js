69988e6999b8991cf64547ae9ae8790dbfc3ef17
kass8mal8 <kassimaly21@gmail.com>
2022-05-12 08:22:59 +0300

    1 file modified

--- src/Blogs.js
+++ src/Blogs.js
@@ -4,7 +4,7 @@ import {useState}  from  'react'
 
 const Blogs=()=>{
   const [blogs, setBlogs] =useState ([
-    {title:"Day 1:Youtube Videos ",body:"Watched Tutorials on YouTube from The Net Ninja and Coding Train. The Tutorial from Net Ninja was named The Complete React Tutorial and it really made me understand much of the concepts in React. The Coding Train was of use to me by mostly referring from its videos on JavaScript array methods such as map, filter, sort and reduce.", date:"1/5/2022",id:1, src:" https://i.ytimg.com/vi/AVQzfUNFudU/maxresdefault.jpg"},
+    {title:"Day 1:Youtube Videos ",body:"Watched Tutorials on YouTube from The Net Ninja and The Coding Train.The videos we're of use to me by mostly referring on JavaScript array methods such as map, filter, sort and reduce.", date:"1/5/2022",id:1, src:" https://i.ytimg.com/vi/AVQzfUNFudU/maxresdefault.jpg"},
     {title:"Day 2:Sololearn Tutorials ",body:"I enrolled for a course in Sololearn entitled React Redux. The course consisted of 23 lessons. I was able to go through all the lessons. At the end of Day Two I had some knowledge on the basics of React even though the Redux part was still confusing to me.", date:"4/5/2022",id:2, src:" https://i.insider.com/60f8275d0729770012b9084d?width=600&format=jpeg&auto=webp"},
     {title:"Day 3:Pdf Tutorials",body:"I downloaded various pdf tutorials from pdf drive some of them being Practical ReactJs and Building Modern Websites Using React. ", date:"8/5/2022",id:3, src:" https://i.ytimg.com/vi/vrRJor7R66A/maxresdefault.jpg"},
     {title:"Day 4:React Project Start",body:" Hello", date:"10/5/2022",id:4, src:"https://wallpaperaccess.com/full/3949102.jpg "},
